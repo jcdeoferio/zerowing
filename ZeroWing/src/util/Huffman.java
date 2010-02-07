34c5c3a7e70e8fa6ea5e33882988a8d517a6534a
kevinzana@gmail.com <kevinzana@gmail.com@c557b8b8-138c-11df-a144-db177bfa3603>
2010-02-07 08:37:03 +0000

    Finalized CharacterManipulator. cleaned up some Huffman.java code.
    CharacterManipulator is now ready to use.

--- ZeroWing/src/util/Huffman.java
+++ ZeroWing/src/util/Huffman.java
@@ -63,7 +63,7 @@ public class Huffman {
 		}
 		TreeSet<HuffEnt> ts = toCollection(map);
 		
-		System.out.println("====== "+ts.size());
+//		System.out.println("====== "+ts.size());
 		if(ts.size()<2)return ts;
 		
 		while(ts.size()!=2){

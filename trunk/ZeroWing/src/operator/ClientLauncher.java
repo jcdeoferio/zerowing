5ca9bd5a8a97bc2a30336c16e02a874294d97d3c
jc.deoferio@gmail.com <jc.deoferio@gmail.com@c557b8b8-138c-11df-a144-db177bfa3603>
2010-02-08 14:51:06 +0000

    added operator.ClientMain gui component for managing the client when online.

--- trunk/ZeroWing/src/operator/ClientLauncher.java
+++ trunk/ZeroWing/src/operator/ClientLauncher.java
@@ -16,7 +16,6 @@ import client.*;
 import settings.SystemSettings;
 import signal.ServerConnection;
 
-
 //zerodba zerodbb zerodbc
 //zerodbs1 zerodbs2
 
@@ -39,9 +38,9 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 	JTextField dbPort;
 	JTextField dbUser;
 	JPasswordField dbPassword;
-	
+
 	JTextField[] fieldArray;
-	
+
 	JPanel changeUnitControlPanel;
 	JTextField column;
 	JTextField table;
@@ -55,11 +54,11 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 	JButton refreshPeerList;
 	JButton sync;
 	JButton trackerConnect;
-	
+
 	JButton setChangeUnit;
 
-	static String previousLaunch = "clientStartupSettings.txt"; 
-	
+	static String previousLaunch = "clientStartupSettings.txt";
+
 	public ClientLauncher() {
 		panel = new JPanel();
 		startClient = new JButton("Start Client");
@@ -79,17 +78,17 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		ip = new JTextField("127.0.0.1");
 		port = new JTextField("8888");
 		dbName = new JTextField("zerodb");
-		
-		dbType = new JComboBox(new String[]{"postgresql", "mysql"});
+
+		dbType = new JComboBox(new String[] { "postgresql", "mysql" });
 		dbIP = new JTextField("localhost");
 		dbPort = new JTextField("5432");
 		dbUser = new JTextField("postgres");
-		
+
 		menuBar = new JMenuBar();
 		menuBar.add(new JMenu("Menu"));
 		menuBar.setSize(500, 10);
-		
-		//fill field array.
+
+		// fill field array.
 		fillFields();
 
 		panel.add(new JLabel("name:"));
@@ -114,7 +113,7 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		panel.add(startClient);
 
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
+
 		this.setLayout(new FlowLayout(FlowLayout.CENTER));
 		this.add(panel);
 		this.setJMenuBar(menuBar);
@@ -125,11 +124,13 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Main");
 		this.setVisible(true);
-		
+
 		loadFromSettings();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see operator.ClientInterface#startClient()
 	 */
 	public void startClient() throws SQLException {
@@ -138,7 +139,7 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		this.add(panel, BorderLayout.NORTH);
 		panel.removeAll();
 
-		updatesList = new JTextArea(5,20);
+		updatesList = new JTextArea(5, 20);
 		updatesList.setText("");
 		updatesList.setEditable(false);
 		JButton updateButton = new JButton("Data");
@@ -158,78 +159,82 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		panel.add(trackerConnect = new JButton("Connect to Tracker"));
 
 		peerIP.requestFocus();
-//		panel.add(updateButton = new JButton("Data"));
+		// panel.add(updateButton = new JButton("Data"));
 		updatesListInput.add(updateButton, BorderLayout.WEST);
 		updatesListInput.add(updateInput, BorderLayout.CENTER);
-		updateInput.setToolTipText("Format of query must satisfy this format:\n SELECT * FROM [YOUR QUERY]");
-		
+		updateInput
+				.setToolTipText("Format of query must satisfy this format:\n SELECT * FROM [YOUR QUERY]");
+
 		changeUnitControlPanel = new JPanel();
-		changeUnitControlPanel.setLayout(new BoxLayout(changeUnitControlPanel,BoxLayout.Y_AXIS));
+		changeUnitControlPanel.setLayout(new BoxLayout(changeUnitControlPanel,
+				BoxLayout.Y_AXIS));
 		column = new JTextField();
 		table = new JTextField();
 		changeUnit = new JTextField();
-		
-		
-		
+
 		setChangeUnit = new JButton("Set New Change Unit");
-		
-		 changeUnitControlPanel.add(column);
-		 changeUnitControlPanel.add(table);
-		 changeUnitControlPanel.add(changeUnit);
-		
-		 column.setToolTipText("Column Name");
-		 table.setToolTipText("Table Name");
-		 changeUnit.setToolTipText("Change Unit to be Acquired.");
-		 
-		setChangeUnit.addActionListener(new ActionListener(){
+
+		changeUnitControlPanel.add(column);
+		changeUnitControlPanel.add(table);
+		changeUnitControlPanel.add(changeUnit);
+
+		column.setToolTipText("Column Name");
+		table.setToolTipText("Table Name");
+		changeUnit.setToolTipText("Change Unit to be Acquired.");
+
+		setChangeUnit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				callSetChangeUnit(column.getText(),
-						table.getText(), changeUnit.getText());
+				callSetChangeUnit(column.getText(), table.getText(), changeUnit
+						.getText());
 			}
 		});
-		 
+
 		panel.add(changeUnitControlPanel);
 		panel.add(setChangeUnit);
 		panel.add(updatesListInput);
 		panel.add(new JLabel("Updates list"));
 		jps = new JScrollPane();
-		//jps.setPreferredSize(new Dimension(150,100));
-		jps.setSize(new Dimension(150,100));
+		// jps.setPreferredSize(new Dimension(150,100));
+		jps.setSize(new Dimension(150, 100));
 		jps.setViewportView(updatesList);
-		jps.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		jps.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		this.add(jps,BorderLayout.CENTER);
+		jps
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		jps
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		this.add(jps, BorderLayout.CENTER);
 
-
-		updateButton.addActionListener(new ActionListener(){
+		updateButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					updatesList.setText(c.dumpTable(updateInput.getText()));
 				} catch (SQLException e) {
 					e.printStackTrace();
-					updatesList.setText("SQL Exception: "+e.getLocalizedMessage());
+					updatesList.setText("SQL Exception: "
+							+ e.getLocalizedMessage());
 				}
-			}});
-		
+			}
+		});
 
-		updateInput.addActionListener(new ActionListener(){
+		updateInput.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					updatesList.setText(c.dumpTable(updateInput.getText()));
 				} catch (SQLException e) {
 					e.printStackTrace();
-					updatesList.setText("SQL Exception: "+e.getLocalizedMessage());
+					updatesList.setText("SQL Exception: "
+							+ e.getLocalizedMessage());
 				}
-			}});
-		
+			}
+		});
+
 		peerList.setEditable(false);
-		
+
 		trackerConnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				connectToTracker();
 			}
 		});
-		
+
 		sync.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				sync();
@@ -248,24 +253,30 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 			}
 		});
 
-		c = Client.getClient(name.getText(), ip.getText(), Integer.parseInt(port
-				.getText()), (String)dbType.getSelectedItem(), dbIP.getText(), Integer
-				.parseInt(dbPort.getText()), dbName.getText(),
-				dbUser.getText(), new String(dbPassword.getPassword()));
-		if(c==null){
-			System.out.println("Possible SQL Error, see console log for details.");
+		c = Client.getClient(name.getText(), ip.getText(), Integer
+				.parseInt(port.getText()), (String) dbType.getSelectedItem(),
+				dbIP.getText(), Integer.parseInt(dbPort.getText()), dbName
+						.getText(), dbUser.getText(), new String(dbPassword
+						.getPassword()));
+		if (c == null) {
+			System.out
+					.println("Possible SQL Error, see console log for details.");
 			setButtonEnableness(panel, false, 0);
-		} else{
+		} else {
 			c.startClient();
-			this.setTitle(c.getDBName()+": Logged in as: "+c.getDBUser());
+			this.setTitle(c.getDBName() + ": Logged in as: " + c.getDBUser());
 			refreshPeerList();
 		}
-		
+
 	}
-	private void callSetChangeUnit(String column, String table, String cuid){
-		JOptionPane.showMessageDialog(this, "Called Set Change Unit: of col ["+column+"] tab ["+table+"] id: "+cuid);
-		if(column==null || table == null || cuid == null)return;
-		if(column.equals("") || table.equals("") || cuid.equals("")) return;
+
+	private void callSetChangeUnit(String column, String table, String cuid) {
+		JOptionPane.showMessageDialog(this, "Called Set Change Unit: of col ["
+				+ column + "] tab [" + table + "] id: " + cuid);
+		if (column == null || table == null || cuid == null)
+			return;
+		if (column.equals("") || table.equals("") || cuid.equals(""))
+			return;
 		try {
 			c.getDatabase().addToChangeUnit(column, table, cuid);
 		} catch (IllegalArgumentException e) {
@@ -276,17 +287,16 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	private void setButtonEnableness(Component c,boolean enabled, int depth){
-//		for(int i=0;i<depth;i++)System.out.print(" ");
-		if(c instanceof Container){
-			Container b = (Container)c;
+
+	private void setButtonEnableness(Component c, boolean enabled, int depth) {
+		// for(int i=0;i<depth;i++)System.out.print(" ");
+		if (c instanceof Container) {
+			Container b = (Container) c;
 			Component[] comps = b.getComponents();
-//			System.out.println(comps.length + " " + c);
-			
-			for(Component cc : comps){
-				setButtonEnableness( cc, enabled, depth+1);
+			// System.out.println(comps.length + " " + c);
+
+			for (Component cc : comps) {
+				setButtonEnableness(cc, enabled, depth + 1);
 				cc.setEnabled(enabled);
 			}
 		} else {
@@ -294,39 +304,45 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see operator.ClientInterface#refreshPeerList()
 	 */
 	public void refreshPeerList() {
 		c.requestPeerList();
 
-		while(peerList.getItemCount()>0)
+		while (peerList.getItemCount() > 0)
 			peerList.removeItemAt(0);
-		
+
 		for (AddressPort ap : c.candidates) {
 			peerList.addItem(ap);
 		}
 
-		if(peerList.getItemCount()==0)peerList.addItem(new AddressPort(c.getName(),"localhost",c.getClientPeerServerPort()));
+		if (peerList.getItemCount() == 0)
+			peerList.addItem(new AddressPort(c.getName(), "localhost", c
+					.getClientPeerServerPort()));
 	}
-	
-	public void addPeer(){
+
+	public void addPeer() {
 		addPeer(peerIP.getText(), Integer.parseInt(peerPort.getText()));
 	}
 
-	public void addPeer(String ip, int port ) {
+	public void addPeer(String ip, int port) {
 		c.addPeerManual(ip, port);
 		refreshPeerList();
 	}
-	
-	public void sync(){
+
+	public void sync() {
 		c.ask();
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see operator.ClientInterface#connectToTracker()
 	 */
-	public void connectToTracker(){
+	public void connectToTracker() {
 		c.loginTracker();
 	}
 
@@ -340,43 +356,40 @@ public class ClientLauncher extends JFrame implements ClientInterface {
 		@SuppressWarnings("unused")
 		ClientInterface m = new ClientLauncher();
 	}
-		
-	private void fillFields(){
-		fieldArray = new JTextField[]{
-				name,
-				ip,
-				port,
-				dbName,
-				dbIP,
-				dbPort,
-				dbUser
-			};
+
+	private void fillFields() {
+		fieldArray = new JTextField[] { name, ip, port, dbName, dbIP, dbPort,
+				dbUser };
 	}
-	private void loadFromSettings(){
-		String[] fieldData = 
-			SystemSettings.getSystemSettingsArray(ClientLauncher.previousLaunch);
-		
+
+	private void loadFromSettings() {
+		String[] fieldData = SystemSettings
+				.getSystemSettingsArray(ClientLauncher.previousLaunch);
+
 		loadFields(fieldData);
 	}
-	private void saveSettings(){
-		System.out.println("ClientLauncher.saveSettings(): attempting to save this launch.");
+
+	private void saveSettings() {
+		System.out
+				.println("ClientLauncher.saveSettings(): attempting to save this launch.");
 		String[] toSave = new String[fieldArray.length];
-		for(int i=0; i< fieldArray.length;i++){
+		for (int i = 0; i < fieldArray.length; i++) {
 			toSave[i] = fieldArray[i].getText();
 		}
-		SystemSettings.insertSettingsAsArray(toSave, ClientLauncher.previousLaunch);
+		SystemSettings.insertSettingsAsArray(toSave,
+				ClientLauncher.previousLaunch);
 	}
-	
-	private void loadFields(String[] fieldData){
-		if(fieldData==null){
+
+	private void loadFields(String[] fieldData) {
+		if (fieldData == null) {
 			return;
 		}
-		if(fieldArray.length==fieldData.length){
-			for(int i=0; i< fieldArray.length;i++){
-				
+		if (fieldArray.length == fieldData.length) {
+			for (int i = 0; i < fieldArray.length; i++) {
+
 				fieldArray[i].setText(fieldData[i]);
 			}
 		}
 	}
-	
+
 }

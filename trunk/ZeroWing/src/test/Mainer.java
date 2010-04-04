3112c3f8500036ebfcaf26d5de5d771ad512851e
jc.deoferio@gmail.com <jc.deoferio@gmail.com@c557b8b8-138c-11df-a144-db177bfa3603>
2010-04-04 09:30:16 +0000

    updated test.Mainer to allow custom port numbers.

--- trunk/ZeroWing/src/test/Mainer.java
+++ trunk/ZeroWing/src/test/Mainer.java
@@ -2,12 +2,23 @@ package test;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import signal.Tracker;
 
 public class Mainer {
 	public static void main(String[] args){
 		Tracker servConn;
-		servConn = new Tracker(8888, "MainerTracker");
+		int port = -1;
+		while(port == -1){
+			String strPort = JOptionPane.showInputDialog(null, "Tracker Port Number:", "8888");
+			try{
+				port = Integer.parseInt(strPort);
+			}catch(Exception e){
+				port = -1;
+			}
+		}
+		servConn = new Tracker(port, "MainerTracker");
 		
 		try {
 			servConn.startServer();
